086c540df09c93d6e3c9b0745e8890be6948ff8d
Brian Blais <bblais@gmail.com>
2025-09-09 20:54:41 -0400

    misc

--- Sprint #1 - Programming and Games/2025-09-09 - Class Misc.py
+++ Sprint #1 - Programming and Games/2025-09-09 - Class Misc.py
@@ -69,24 +69,106 @@ pendown()
 square(20)
 
 
+# In[ ]:
+
+
+from random import randint,choice
+
+
 # In[19]:
 
 
 reset()
 
 penup()
-goto( randint(-100,100), randint(-100,100))
+x,y=randint(-100,100), randint(-100,100)
+goto(x,y)
 pendown()
 
+# if x>0 and y>0:
+#     pencolor("red")
+
+# random color
 pencolor(choice(["red","green","blue","black","orange","purple"]))
 square(20)
 
 penup()
-goto( randint(-100,100), randint(-100,100))
+x,y=randint(-100,100), randint(-100,100)
+goto(x,y)
 pendown()
 
 pencolor(choice(["red","green","blue","black","orange","purple"]))
 square(20)
+
+
+# In[37]:
+
+
+randint(200,300)
+
+
+# In[28]:
+
+
+bob=3
+
+if bob>10:
+    print("big")
+    print("huge")
+elif bob>6:
+    print("medium")
+else:
+    print("small")
+    print("tiny")
+
+print("done")
+
+
+# In[29]:
+
+
+if shape=="square":
+    do_something()
+elif shape=="triangle":
+    do_something_else()
+elif shape=="pentagon":
+    do_another_thing()
+else:
+    print("Invalid shape")
+
+
+# In[30]:
+
+
+def twice_with_return(x):
+    return x*2
+
+def twice_with_print(x):
+    print(x*2)
+
+
+# In[31]:
+
+
+twice_with_print(5)
+
+
+# In[32]:
+
+
+twice_with_return(5)
+
+
+# In[33]:
+
+
+twice_with_return(5)*10
+
+
+# In[34]:
+
+
+twice_with_print(5)*10
 
 
 # In[ ]:
