99fc484d39ec44af15b25e59f6d958f43037b861
Brian Blais <bblais@gmail.com>
2025-09-12 12:33:58 -0400

    class

--- Sprint #1 - Programming and Games/2025-09-12 Class2.py
+++ Sprint #1 - Programming and Games/2025-09-12 Class2.py
@@ -72,8 +72,68 @@ def valid_moves(state,player):
     return moves
 
 
+# In[12]:
+
+
+def valid_moves(state,player):
+
+    moves=[]
+
+    for location in range(9):
+        if state[location]==0: 
+            move=["S",location]
+            moves.append(move)
+
+
+
+    return moves
+
+
+# In[13]:
+
+
+state=Board(3,3)
+state[2]=1
+state[4]=2
+state
+
+
+# In[14]:
+
+
+valid_moves(state,1)
+
+
+# In[18]:
+
+
+for i in range(9):
+    if i==5:
+        break
+    print(i,end=" ")
+
+
+# In[19]:
+
+
+for i in range(9):
+    if i==5:
+        continue
+    print(i,end=" ")
+
+
 # In[ ]:
 
 
+def win_status(state,player):
 
+
+    if player==1:
+        for location in [0,1,2,3,4,5,6,7,8,9,
+                         10,11,12,13,14,15,16,17,18,19]:
+            if state[location]==1 and state[location+5]==1 and state[location+10]==1 and state[location+15]==1:
+                return "win"
+
+
+    return moves
 
