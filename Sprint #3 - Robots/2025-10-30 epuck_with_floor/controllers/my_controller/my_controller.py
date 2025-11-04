8c2a5e8f63fbc00c8a5e8c612556db09f3830780
Brian Blais <bblais@gmail.com>
2025-11-04 13:17:53 -0500

    arm motion

--- Sprint #3 - Robots/2025-10-30 epuck_with_floor/controllers/my_controller/my_controller.py
+++ Sprint #3 - Robots/2025-10-30 epuck_with_floor/controllers/my_controller/my_controller.py
@@ -19,24 +19,54 @@ timestep = int(robot.getBasicTimeStep())
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 
-# Get number of devices
 num_devices = robot.getNumberOfDevices()
-
 print(f"Total devices: {num_devices}\n")
-# List all devices
+print("="*70)
+
 for i in range(num_devices):
     device = robot.getDeviceByIndex(i)
-    print(f"Device {i}: {device.getName()} (Type: {device.getNodeType()})")
+    name = device.getName()
+    node_type = device.getNodeType()
+    
+    print(f"\nDevice {i}: {name}")
+    print(f"  Type: {node_type}")
 
+left_motor = robot.getDevice("left wheel motor")
+right_motor = robot.getDevice("right wheel motor")
+left_motor.setPosition(float("inf"))
+right_motor.setPosition(float("inf"))
+left_motor.setVelocity(0.0)
+right_motor.setVelocity(0.0)
+
+gs0 = robot.getDevice("gs0")
+gs0.enable(timestep)
+
+gs1 = robot.getDevice("gs1")
+gs1.enable(timestep)
+
+gs2 = robot.getDevice("gs2")
+gs2.enable(timestep)
+
+
+# Main loop:
+# - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
 
     # Process sensor data here.
+    left_motor.setVelocity(3.28)
+    right_motor.setVelocity(3.28)
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
-    pass
+    
+    value0=gs0.getValue()
+    value1=gs1.getValue()
+    value2=gs2.getValue()
+    
+    print(f"{value0:.1f},{value1:.1f},{value2:.1f}")
 
 # Enter here exit cleanup code.
+
--- Sprint #3 - Robots/p-rob3/controllers/my_controller/my_controller.py
+++ Sprint #3 - Robots/p-rob3/controllers/my_controller/my_controller.py
@@ -1,8 +1,22 @@
-"""supervisor_controller controller."""
+"""my_controller controller."""
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+
+
+def wait(sec):
+    start_time = robot.getTime()
+    while start_time + sec > robot.getTime():
+        val=robot.step(timestep)
+        if val==-1:
+            return -1
+
+def open_gripper():
+    motors['gripper::right'].setPosition(0.5)
+
+def close_gripper():
+    motors['gripper::right'].setTorque(-0.2)
 
 # create the Robot instance.
 robot = Robot()
@@ -16,35 +30,45 @@ timestep = int(robot.getBasicTimeStep())
 #  ds = robot.getDevice('dsname')
 #  ds.enable(timestep)
 
-# Get number of devices
 num_devices = robot.getNumberOfDevices()
-
 print(f"Total devices: {num_devices}\n")
+print("="*70)
 
-# List all devices
 for i in range(num_devices):
     device = robot.getDeviceByIndex(i)
-    print(f"Device {i}: {device.getName()} (Type: {device.getNodeType()})")
+    name = device.getName()
+    node_type = device.getNodeType()
+    
+    print(f"\nDevice {i}: {name}")
+    print(f"  Type: {node_type}")
 
-left_motor = robot.getDevice("left wheel motor")
-right_motor = robot.getDevice("right wheel motor")
-left_motor.setPosition(float("inf"))
-right_motor.setPosition(float("inf"))
-left_motor.setVelocity(0.0)
-right_motor.setVelocity(0.0)
+motors={}
+for name in [f'motor {i}' for i in range(1,7)]+['gripper::right','gripper::left']:
+    motors[name]=robot.getDevice(name)
 
-
+print(motors.keys())
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
+    wait(.5)
+    close_gripper()
+    wait(.5)
+    open_gripper()
+    wait(.5)
+    close_gripper()
+
+    motors['motor 1'].setPosition(1.55)
+    motors['motor 2'].setPosition(1.12)
+    open_gripper()
+    wait(1.5)
+
+
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
 
     # Process sensor data here.
-    left_motor.setVelocity(3.28)
-    right_motor.setVelocity(3.28)
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
